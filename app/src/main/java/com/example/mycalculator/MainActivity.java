6f501ca23fe2876da005dafc33ba617dbaf13eb6
Vladimir Grigoriev <intvreporting@gmail.com>
2022-01-29 17:05:12 +0200

    Настроен main_activity для работы с классом калькулятор

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final Calc calc = new Calc();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        init();
+        setListeners();
     }
+
+    public void init() {
+        for (int i = 0; i < Calc.ExistButtons.values().length; i++){
+            calc.setButton(findViewById(Calc.ExistButtons.values()[i].getLayoutID()),i);
+        }
+        calc.setSummaries(findViewById(R.id.summaries));
+    }
+
+    public void setListeners(){
+        calc.setListeners();
+    }
+
+
+
 }
 
 
